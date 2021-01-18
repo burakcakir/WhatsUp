dce567e0b005ce7aece0dfc0201816b7e82b56ce
Burak Yüksel <44025823+burakyuksel09@users.noreply.github.com>
2021-01-18 11:48:17 +0300

    v 1.3
    giriş yap ve kayıtl ol kısmı tasarım yapıldı

--- app/src/main/java/com/example/whatsup/GirisYapActivity.java
+++ app/src/main/java/com/example/whatsup/GirisYapActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -21,7 +22,8 @@ public class GirisYapActivity extends AppCompatActivity {
     ImageView imageView;
     EditText kullaniciMail;
     EditText kullaniciSifre;
-    ImageButton kayitolButton;
+    Button kayitolButton;
+
 
     private FirebaseAuth firebaseAuth;
 
--- app/src/main/java/com/example/whatsup/KayitOlActivity.java
+++ app/src/main/java/com/example/whatsup/KayitOlActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -21,7 +22,7 @@ public class KayitOlActivity extends AppCompatActivity {
     ImageView imageView;
     EditText kullaniciMail;
     EditText kullaniciSifre;
-    ImageButton kayitolButton;
+    Button kayitolButton;
 
     private FirebaseAuth firebaseAuth;
 
