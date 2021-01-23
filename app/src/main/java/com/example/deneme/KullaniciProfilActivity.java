bce4a37a5ce81852a825dcad3f0550c2500aeaac
Burak Yüksel <44025823+burakyuksel09@users.noreply.github.com>
2021-01-23 12:30:01 +0300

    v1.1
    profile geçiş kısmı düzeltildi

---
+++ app/src/main/java/com/example/deneme/KullaniciProfilActivity.java
@@ -0,0 +1,23 @@
+package com.example.deneme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class KullaniciProfilActivity extends AppCompatActivity {
+
+    public void guncelle(View view){
+
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kullanici_profil);
+    }
+}
